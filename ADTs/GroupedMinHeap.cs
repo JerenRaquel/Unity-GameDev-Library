b8fd53e8ef7951fb51187e0d0d79584ab7dcdf0b
Jeren Raquel <jeraquel101@gmail.com>
2022-12-26 14:10:35 -0800

    Updated MinHeap and Added a group variant

---
+++ ADTs/GroupedMinHeap.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+
+/*
+    Description: This is a data structure that acts like a MinHeap and Dictionary<ID, List>.
+
+    Notes: The initial purpose of this data structure is to hold a group of items for a "key" 
+        acting as an ID. However, the keys are organized in min ordered by ID. The ID is defined
+        on the size of each element. Each group will be fetched by random order from that group.
+
+    Docs:
+        For the constructor:
+            maxKeySize: The max amount of keys that can be held
+            lengthIdentifier: How to evaluate the size of an element
+        IsEmpty(): Checks if there is no data stored
+        Count(): Returns the amount of added items
+        Peek(): Returns a random T from the smallest group, if applicable
+        Pop(): Returns a random T from the smallest group and removes it from the group, 
+            if applicable
+        Add(T): Adds to a group
+        Find(T): returns true if T was found
+*/
+
+public class GroupedMinHeap<T> {
+    public delegate int Length(T item);
+
+    public int Count { get; private set; } = 0;
+
+    private int maxKeySize;
+    private Dictionary<int, List<T>> data;
+    private MinHeap<int> keys;
+    private Length lengthIdentifier;
+    private System.Random rng;
+
+    public GroupedMinHeap(int maxKeySize, Length lengthIdentifier) {
+        this.data = new Dictionary<int, List<T>>();
+        this.keys = new MinHeap<int>(
+            maxKeySize,
+            (int LHS, int RHS) => LHS < RHS,
+            (int LHS, int RHS) => LHS == RHS
+        );
+        this.rng = new System.Random();
+        this.lengthIdentifier = lengthIdentifier;
+        this.maxKeySize = maxKeySize;
+    }
+
+    public bool IsEmpty() { return (this.Count == 0); }
+
+    public T Peek() {
+        if (this.Count <= 0) {
+            throw new System.IndexOutOfRangeException("No Data Availiable");
+        }
+
+        int key = this.keys.Peek();
+        int amt = this.data[key].Count;
+        int rngIndex = this.rng.Next(0, amt + 1);
+        return this.data[key][rngIndex];
+    }
+
+    public T Pop() {
+        T item = this.Peek();
+        this.data[this.keys.Peek()].Remove(item);
+        this.Count--;
+
+        if (this.data[this.keys.Peek()].Count <= 0) {
+            this.keys.Pop();
+        }
+
+        return item;
+    }
+
+    public void Add(T item) {
+        int key = this.lengthIdentifier(item);
+
+        if (!this.keys.Find(key)) {
+            if (this.keys.Count >= this.maxKeySize) {
+                throw new System.IndexOutOfRangeException("Can't Hold any more unique data");
+            }
+            if (!this.data.ContainsKey(key)) {
+                this.data.Add(key, new List<T>());
+            }
+            this.keys.Add(key);
+        }
+
+        this.data[key].Add(item);
+        this.Count++;
+    }
+
+    public bool Find(T item) {
+        if (this.Count <= 0) return false;
+
+        // Stop once found, returns true if never found
+        return !this.keys.Every((in int key) => {
+            return !this.data[key].Contains(item);
+        });
+    }
+}
--- ADTs/MinHeap.cs
+++ ADTs/MinHeap.cs
@@ -14,122 +14,132 @@
                       T to be seen as the smaller one. [Classes and structs are supported this way]
         equal: This is the same as the lessThanFunc, but to determine equality.
       IsEmpty(): Checks if the heap is empty
+      Count(): Returns the amount of added items
       Peek(): Returns the smallest T in the heap, if applicable
       Pop(): Returns the smallest T in the heap and removes it from the heap, if applicable
       Add(T): Adds to the heap
       Find(T): returns true if T was found in the heap
+      ForEach(Accessor accessor(in T)): Apply a function on each element; every element is immutable
+      Every(Accessor accessor(in T)): Apply a conditional function on each immutable element; stop on the first false state
  */
 
-public class MinHeap<T>
-{
-  public delegate bool Comparator(T lhs, T rhs);
+public class MinHeap<T> {
+    public delegate bool Comparator(T lhs, T rhs);
+    public delegate bool Accessor(in T item);
 
-  private Comparator _comparatorLess;
-  private Comparator _comparatorEqual;
-  private readonly T[] _elements;
-  private int _size;
+    private Comparator _comparatorLess;
+    private Comparator _comparatorEqual;
+    private readonly T[] _elements;
+    private int _size;
 
-  public MinHeap(int size, Comparator lessThanFunc, Comparator equal)
-  {
-    _elements = new T[size];
-    _comparatorLess = lessThanFunc;
-    _comparatorEqual = equal;
-  }
-
-  private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
-  private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
-  private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
-
-  private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < _size;
-  private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < _size;
-  private bool IsRoot(int elementIndex) => elementIndex == 0;
-
-  private T GetLeftChild(int elementIndex) => _elements[GetLeftChildIndex(elementIndex)];
-  private T GetRightChild(int elementIndex) => _elements[GetRightChildIndex(elementIndex)];
-  private T GetParent(int elementIndex) => _elements[GetParentIndex(elementIndex)];
-
-  private void Swap(int firstIndex, int secondIndex)
-  {
-    var temp = _elements[firstIndex];
-    _elements[firstIndex] = _elements[secondIndex];
-    _elements[secondIndex] = temp;
-  }
-
-  public bool IsEmpty() { return (_size == 0); }
-
-  public T Peek()
-  {
-    if (_size == 0)
-      throw new IndexOutOfRangeException();
-
-    return _elements[0];
-  }
-
-  public T Pop()
-  {
-    if (_size == 0)
-      throw new IndexOutOfRangeException();
-
-    var result = _elements[0];
-    _elements[0] = _elements[_size - 1];
-    _size--;
-
-    ReCalculateDown();
-
-    return result;
-  }
-
-  public void Add(T element)
-  {
-    if (_size == _elements.Length)
-      throw new IndexOutOfRangeException();
-
-    _elements[_size] = element;
-    _size++;
-
-    ReCalculateUp();
-  }
-
-  public bool Find(T element)
-  {
-    if (_size == 0)
-      return false;
-
-    for (int i = 0; i < _size; i++)
-    {
-      if (_comparatorEqual(element, _elements[i]))
-        return true;
+    public MinHeap(int size, Comparator lessThanFunc, Comparator equal) {
+        _elements = new T[size];
+        _comparatorLess = lessThanFunc;
+        _comparatorEqual = equal;
     }
-    return false;
-  }
 
-  private void ReCalculateDown()
-  {
-    int index = 0;
-    while (HasLeftChild(index))
-    {
-      var smallerIndex = GetLeftChildIndex(index);
-      if (HasRightChild(index) && _comparatorLess(GetRightChild(index), GetLeftChild(index)))
-      {
-        smallerIndex = GetRightChildIndex(index);
-      }
+    private int GetLeftChildIndex(int elementIndex) => 2 * elementIndex + 1;
+    private int GetRightChildIndex(int elementIndex) => 2 * elementIndex + 2;
+    private int GetParentIndex(int elementIndex) => (elementIndex - 1) / 2;
 
-      if (!_comparatorLess(_elements[smallerIndex], _elements[index]))  // !(_elements[smallerIndex] >= _elements[index])
-        break;
+    private bool HasLeftChild(int elementIndex) => GetLeftChildIndex(elementIndex) < _size;
+    private bool HasRightChild(int elementIndex) => GetRightChildIndex(elementIndex) < _size;
+    private bool IsRoot(int elementIndex) => elementIndex == 0;
 
-      Swap(smallerIndex, index);
-      index = smallerIndex;
+    private T GetLeftChild(int elementIndex) => _elements[GetLeftChildIndex(elementIndex)];
+    private T GetRightChild(int elementIndex) => _elements[GetRightChildIndex(elementIndex)];
+    private T GetParent(int elementIndex) => _elements[GetParentIndex(elementIndex)];
+
+    private void Swap(int firstIndex, int secondIndex) {
+        var temp = _elements[firstIndex];
+        _elements[firstIndex] = _elements[secondIndex];
+        _elements[secondIndex] = temp;
     }
-  }
 
-  private void ReCalculateUp()
-  {
-    var index = _size - 1;
-    while (!IsRoot(index) && _comparatorLess(_elements[index], GetParent(index)))
-    {
-      var parentIndex = GetParentIndex(index);
-      Swap(parentIndex, index);
-      index = parentIndex;
+    public bool IsEmpty() { return (_size == 0); }
+
+    public int Count { get { return _size; } }
+
+    public T Peek() {
+        if (_size == 0)
+            throw new IndexOutOfRangeException();
+
+        return _elements[0];
     }
-  }
+
+    public T Pop() {
+        if (_size == 0)
+            throw new IndexOutOfRangeException();
+
+        var result = _elements[0];
+        _elements[0] = _elements[_size - 1];
+        _size--;
+
+        ReCalculateDown();
+
+        return result;
+    }
+
+    public void Add(T element) {
+        if (_size == _elements.Length)
+            throw new IndexOutOfRangeException();
+
+        _elements[_size] = element;
+        _size++;
+
+        ReCalculateUp();
+    }
+
+    public bool Find(T element) {
+        if (_size == 0)
+            return false;
+
+        for (int i = 0; i < _size; i++) {
+            if (_comparatorEqual(element, _elements[i]))
+                return true;
+        }
+        return false;
+    }
+
+    public void ForEach(Accessor accessor) {
+        for (int i = 0; i < _size; i++) {
+            accessor(_elements[i]);
+        }
+    }
+
+    public bool Every(Accessor accessor) {
+        bool trueForAll = true;
+        for (int i = 0; i < _size; i++) {
+            if (!accessor(_elements[i])) {
+                trueForAll = false;
+                break;
+            }
+        }
+        return trueForAll;
+    }
+
+    private void ReCalculateDown() {
+        int index = 0;
+        while (HasLeftChild(index)) {
+            var smallerIndex = GetLeftChildIndex(index);
+            if (HasRightChild(index) && _comparatorLess(GetRightChild(index), GetLeftChild(index))) {
+                smallerIndex = GetRightChildIndex(index);
+            }
+
+            if (!_comparatorLess(_elements[smallerIndex], _elements[index]))  // !(_elements[smallerIndex] >= _elements[index])
+                break;
+
+            Swap(smallerIndex, index);
+            index = smallerIndex;
+        }
+    }
+
+    private void ReCalculateUp() {
+        var index = _size - 1;
+        while (!IsRoot(index) && _comparatorLess(_elements[index], GetParent(index))) {
+            var parentIndex = GetParentIndex(index);
+            Swap(parentIndex, index);
+            index = parentIndex;
+        }
+    }
 }
