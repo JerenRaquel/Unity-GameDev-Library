7c6eff7309b1a1510e44d4e4a2a780420fa628ca
Jeren Raquel <jeraquel101@gmail.com>
2022-12-27 18:14:44 -0800

    Added a multimap with a random output for each key value pair

---
+++ ADTs/RNGMultimap.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+public class RNGMultimap<K, V> {
+    private Dictionary<K, List<V>> data;
+    private Func<V, V, bool> equalsEvaluator;
+    private Random rng;
+
+    public RNGMultimap(Func<V, V, bool> equalsEvaluator) {
+        this.data = new Dictionary<K, List<V>>();
+        this.equalsEvaluator = equalsEvaluator;
+        this.rng = new Random();
+    }
+
+    public void Add(K key, V value) {
+        if (!this.ContainsKey(key)) {
+            this.data.Add(key, new List<V>());
+        }
+
+        this.data[key].Add(value);
+    }
+
+    public bool Remove(K key, V value) {
+        if (!this.data.ContainsKey(key)) return false;
+
+        return this.data[key].Remove(value);
+    }
+
+    public V this[K key] {
+        get {
+            int index = this.rng.Next(0, this.data[key].Count);
+            return this.data[key][index];
+        }
+    }
+
+    public bool ContainsKey(K key) { return this.data.ContainsKey(key); }
+
+    public bool ContainsValue(V value) {
+        foreach (K key in this.data.Keys) {
+            foreach (V item in this.data[key]) {
+                if (this.equalsEvaluator(item, value)) return true;
+            }
+        }
+        return false;
+    }
+}
